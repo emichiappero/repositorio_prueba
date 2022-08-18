9985871808af30bf1afc73f5065316d64dfa9095
emichiappero <emichiappero@gmail.com>
2022-08-17 21:10:39 -0300

    Primera version de los archivos

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # repositorio_prueba
+
+Este es un repositorio de prueba
---
+++ index.js
@@ -0,0 +1,17 @@
+// 1) Requerimos/incluimos los paquetes necesarios
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// 1.1) Configuraciones
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 2) Rutas
+app.get("/prueba", function (req, res) {
+  res.send("Hola Mundo");
+});
+
+// 3) Listen
+app.listen(4000, function () {
+  console.log("Servidor iniciado");
+});
